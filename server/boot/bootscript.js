d55dc9263528af69d6262f8ad89cd5c40f8e6820
ouyangwanbin <ouyangwanbin@gmail.com>
2015-06-22 16:05:31 -0700

    change the bootscripts , so the admin will not be generated every time

--- server/boot/bootscript.js
+++ server/boot/bootscript.js
@@ -1,28 +1,38 @@
 module.exports = function(app) {
+    var admin = {
+        email: 'admin@gmail.com',
+        password: 'abc123'
+    };
     var User = app.models.User;
     var Role = app.models.Role;
     var RoleMapping = app.models.RoleMapping;
-    User.create([{
-        email: 'admin@gmail.com',
-        password: 'abc123'
-    }], function(err, users) {
-        if (err) {
-            return console.log(err);
-        }
-
-        //create the admin role
-        Role.create({
-            name: 'admin'
-        }, function(err, role) {
-            if (err) {
-                console.log(err);
+    User.count({
+            email: admin.user
+        },
+        function(err , response) {
+            if (response > 0) {
+                console.log('admin already exist.');
+                return false;
             }
-            role.principals.create({
-                principalType: RoleMapping.USER,
-                principalId: users[0].id
-            }, function(err, principal) {
-                console.log(err);
+            User.create([admin], function(err, users) {
+                if (err) {
+                    return console.log(err);
+                }
+
+                //create the admin role
+                Role.create({
+                    name: 'admin'
+                }, function(err, role) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    role.principals.create({
+                        principalType: RoleMapping.USER,
+                        principalId: users[0].id
+                    }, function(err, principal) {
+                        console.log(err);
+                    });
+                });
             });
-        });
-    });
+        })
 }
